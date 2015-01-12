0e80f4a9afdb6a3a851343a7719abbaf1d6892ed
Knut Collin <kc@hin.no>
2015-01-12 15:17:36 +0100

    Fixes in TestErrorFix branch

--- GitTest/src/no/kc/git/first/GitTest.java
+++ GitTest/src/no/kc/git/first/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 	    System.out.println("Git is fun");
 	    System.out.println("Git is fun too.....");
 
+	    System.out.println("Fixes done in TestErrorFix branch");
 	}
 
 }
